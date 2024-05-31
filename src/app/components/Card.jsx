8127f42eaf0c3f9639a902b28ca418d098c6b792
school0531 <tom2004527@gmail.com>
2024-05-31 16:39:48 +0800

    改Card和新增CustomCard的component

---
+++ src/app/components/Card.jsx
@@ -0,0 +1,10 @@
+/**
+ * @author 蔡翔宇 <b11117048@gemail.yuntech.edu.tw>
+ */
+export default function CustomCard({item}){
+    return (
+        <div class="border">
+            <div>{item.ScenicSpotName}</div>
+        </div>
+    )
+}
