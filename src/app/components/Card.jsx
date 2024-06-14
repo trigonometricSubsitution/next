37d62810bcf7d639d34f4afdc470de7a99107d29
school0614 <tom2004527@gmail.com>
2024-06-14 17:05:29 +0800

    new file:   src/app/api/items/route.js 	modified:   src/app/components/Card.jsx 	modified:   src/app/page.js

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,9 +1,12 @@
 /**
  * @author 蔡翔宇 <b11117048@gemail.yuntech.edu.tw>
  */
-export default function CustomCard({item}){
+import Image from "next/image";
+
+export default function Card({item}){
     return (
         <div class="border">
+            <img src={item.Picture.PictureUrl1} alt={item.Picture.PictureDescription1}/>
             <div>{item.ScenicSpotName}</div>
         </div>
     )
--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@
 
 import { useState, useEffect} from 'react';
 import Link from "next/link";
+import Image from 'next/image';
 import { 
   Navbar,
   NavbarBrand, 
@@ -16,37 +17,23 @@ import {
   FooterLink, 
   FooterLinkGroup,
   Carousel,
-  Card,
   Button,
   DarkThemeToggle
 } from "flowbite-react";
-import CustomCard from "./components/Card";
+import Card from "./components/Card";
 
 export default function Home() {
 
   const [items, setItems] = useState([]);
-  const tokenUrl = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token';
-  const apiUrl = 'https://tdx.transportdata.tw/api/basic/v2/Tourism/ScenicSpot/YunlinCounty';
 
   useEffect(() => {
-    const getToken = async () => {
-      const clientId = process.env.TDX_CLIENT_ID;
-      const clientSecret = process.env.TDX_CLIENT_SECRET;
-
-      const tokenParams = new URLSearchParams();
-      tokenParams.append('grant_type', 'client_credentials');
-      tokenParams.append('client_id', clientId);
-      tokenParams.append('client_secret', clientSecret);
-
-      const tokenResponse = await fetch(tokenUrl, {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        body: tokenParams.toString()
-      });
-    };
-
+    async function fetchData() {
+      const response = await fetch('api/items');
+      const data = await response.json();
+      console.log(data);
+      setItems(data);
+    }
+    fetchData();
   }, []);
 
   return (
@@ -84,42 +71,16 @@ export default function Home() {
         </Carousel>
       </div>
 
-      <div className="bg-white py-16">
-        <div className="container mx-auto grid grid-cols-4 gap-4">
-          { items.map( item =>
-          <Card
-            className="max-w-sm"
-            imgAlt={item.Picture.PictureDescription1}
-            imgSrc={item.Picture.PictureUrl1}
-          >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              {item.ScenicSpotName}
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              {item.DescriptionDetail}
-            </p>
-            <Button>
-              Read more
-              <svg className="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </Button>
-          </Card>
-          )}
-        </div>
-      </div>
-      <div class="container mx-auto">
-        <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
           {
-            items.map( item =>
-              <CustomCard item={item}/>
+            items.map( (item, index) =>
+              <Card item={item} key={index}/>
           )}
         </div>
       </div>
+
+      
       <Footer container>
       <FooterCopyright href="#" by="Flowbite™" year={2022} />
       <FooterLinkGroup>
